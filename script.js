b830cf3a6650a53f93514311cbd6c479fc73208d
l3r05a <200935142+L3r05a@users.noreply.github.com>
2025-07-25 09:57:23 +0100

    Add hover and reset functionality

--- script.js
+++ script.js
@@ -1,19 +1,54 @@
 const container = document.querySelector('.container');
 
+const button = document.querySelector('button');
 
-for (let n = 0; n < 16; n++){
+let value =  10;
+
+function eAS(value){
+
+
+
+for (let n = 0; n < value; n++){
 
     const columns = document.createElement('div');
     columns.classList.add('column');
     container.appendChild(columns);
+ 
 
 
-    for (let n = 0; n < 16; n++) {
+    for (let n = 0; n < value; n++) {
         const rows = document.createElement('div');
         rows.classList.add('row');
         columns.appendChild(rows);
-        }
-}
+        rows.addEventListener('mouseenter', () =>{
+            rows.style.backgroundColor = 'yellow'
+        })
+
+
+            button.addEventListener('click', () => {
+
+            rows.style.backgroundColor = 'white';
+
+            
+    })
+        }}
+}     
+       
+eAS(value);
+
+
+
+        // button.addEventListener('click', () => {
+// reset
+// let newValue = prompt ('New Grid?', 16);
+// if (newValue >0 && newValue <100){
+//     value = 0;
+//     eAS(newValue);
+// } else {
+//     prompt ('too much, not enough');
+// }
+
+// })
 
 
 
